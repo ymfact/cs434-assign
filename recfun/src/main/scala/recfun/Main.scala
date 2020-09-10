c7242fca6b8e84df907645389170fae5af2bd89a
ymfact <ymfact@postech.ac.kr>
2020-09-10 23:23:38 +0900

    implement pascal, balance, countChange

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
-import common._
+
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -14,15 +15,64 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r){
+      1
+    }else if(c < 0 || r < c) {
+      0
+    }else{
+      pascal(c-1, r-1) + pascal(c, r-1)
+    }
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  @tailrec
+  def balance(chars: List[Char]): Boolean = {
+    chars match {
+      case List() => true
+      case '(' :: tail => close(tail) match
+      {
+        case Ok(remains) => balance(remains)
+        case Failed() => false
+      }
+      case ')' :: tail => false
+      case _ :: tail => balance(tail)
+    }
+  }
+
+  abstract class ConsumeResult
+  case class Failed() extends ConsumeResult
+  case class Ok(remains: List[Char]) extends ConsumeResult
+
+  def close(chars: List[Char]): ConsumeResult = {
+    chars match {
+      case List() => Failed()
+      case '(' :: tail =>
+        close(tail) match {
+          case Ok(remains) => close(remains)
+          case Failed() => Failed()
+        }
+      case ')' :: tail => Ok(tail)
+      case _ :: tail => close(tail)
+    }
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    coins match{
+      case List() if money == 0 => 1
+      case List() => 0
+      case coinValue :: coinsRemain =>
+        var chargeMethodCount = 0
+        for(coinCount <- 0 to money / coinValue){
+          val moneyRemain = money - coinCount * coinValue
+          chargeMethodCount += countChange(moneyRemain, coinsRemain)
+        }
+        chargeMethodCount
+    }
+  }
 }
