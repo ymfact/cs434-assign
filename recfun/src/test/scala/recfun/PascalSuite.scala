0c9c3f14d3604c17b6100ad5679f0283733b0033
ymfact <ymfact@postech.ac.kr>
2020-09-10 22:13:57 +0900

    recfun template

---
+++ recfun/src/test/scala/recfun/PascalSuite.scala
@@ -0,0 +1,22 @@
+package recfun
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PascalSuite extends AnyFunSuite {
+  import Main.pascal
+  test("pascal: col=0,row=2") {
+    assert(pascal(0,2) === 1)
+  }
+
+  test("pascal: col=1,row=2") {
+    assert(pascal(1,2) === 2)
+  }
+
+  test("pascal: col=1,row=3") {
+    assert(pascal(1,3) === 3)
+  }
+}
